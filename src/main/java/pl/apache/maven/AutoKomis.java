ed9db44a90a7fda321e3b99f10d12bef6105c889
Pawel Malkowski <pawel_malkowski@wp.pl>
2019-03-16 12:18:13 +0100

    New project commit

---
+++ src/main/java/pl/apache/maven/AutoKomis.java
@@ -0,0 +1,20 @@
+package pl.apache.maven;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class AutoKomis {
+
+    public  static List<Samochód> CARS = Arrays.asList(
+
+    new Samochód ()
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
