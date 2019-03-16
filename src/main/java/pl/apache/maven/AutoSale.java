8930fe50171e36d0c541ffa2db31fb4b68dbf017
Pawel Malkowski <pawel_malkowski@wp.pl>
2019-03-16 13:33:21 +0100

    New project commit

--- src/main/java/pl/apache/maven/AutoSale.java
+++ src/main/java/pl/apache/maven/AutoSale.java
@@ -3,6 +3,7 @@ package pl.apache.maven;
 import sun.util.resources.cldr.es.CalendarData_es_AR;
 
 import java.lang.reflect.Array;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -17,16 +18,27 @@ public class AutoSale<Car> {
       System.out.println("Podaj parametry samochodu");
 
       System.out.println("Podaj nazwę marki");
+      String name = userScanner.nextLine();
+
       System.out.println("Podaj kolor");
+      String color = userScanner.nextLine();
+
       System.out.println("Podaj liczbę drzwi");
+      int doors = userScanner.nextInt();
+
       System.out.println("Podaj cenę");
+      BigDecimal price = userScanner.nextBigDecimal();
+
       System.out.println("Podaj przebieg");
+      int mileage = userScanner.nextInt();
+
       System.out.println("Podaj rocznik");
+      int data = userScanner.nextInt();
+
       System.out.println("Podaj prędkość maksymalną");
+      int maxSpeedCar = userScanner.nextInt();
 
     }
-
-
 
     public static void main(String[] args) {
 
