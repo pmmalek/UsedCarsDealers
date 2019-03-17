d339a9cd0de0e2ede3d5d36075054e6bd2dac903
Pawel Malkowski <pawel_malkowski@wp.pl>
2019-03-17 12:30:43 +0100

    Implementation of Sorting

--- src/main/java/pl/apache/maven/SortingCars.java
+++ src/main/java/pl/apache/maven/SortingCars.java
@@ -20,21 +20,34 @@ public class SortingCars {
                 .collect(Collectors.toList());
         return cars;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public static List<Car> sortCarsByDoors() {
+        List<Car> cars = AutoSale.cars.stream()
+                .sorted(Comparator.comparing(Car::getDoors))
+                .collect(Collectors.toList());
+        return cars;
     }
+    public static List<Car> sortCarsByPrice() {
+        List<Car> cars = AutoSale.cars.stream()
+                .sorted(Comparator.comparing(Car::getPrice))
+                .collect(Collectors.toList());
+        return cars;
+    }
+    public static List<Car> sortCarsByMileage() {
+        List<Car> cars = AutoSale.cars.stream()
+                .sorted(Comparator.comparing(Car::getMileage))
+                .collect(Collectors.toList());
+        return cars;
+    }
+    public static List<Car> sortCarsByProductionYear() {
+        List<Car> cars = AutoSale.cars.stream()
+                .sorted(Comparator.comparing(Car::getDate))
+                .collect(Collectors.toList());
+        return cars;
+    }
+    public static List<Car> sortCarsByMaxSpeed() {
+        List<Car> cars = AutoSale.cars.stream()
+                .sorted(Comparator.comparing(Car::getMaxSpeed))
+                .collect(Collectors.toList());
+        return cars;
+    }
+}
